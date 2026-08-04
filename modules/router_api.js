31c434e2e1b5962961abe157622938f74cfddb5d
mariusbrion <marius.brion@icloud.com>
2026-08-04 11:08:17 +0200

    Update router_api.js

--- modules/router_api.js
+++ modules/router_api.js
@@ -53,6 +53,22 @@ export const RouterAPI = {
 
             let coordToId;
 
+            function getDist(la1, lo1, la2, lo2) {
+                const dLat = (la2 - la1) * Math.PI / 180;
+                const dLon = (lo2 - lo1) * Math.PI / 180;
+                const a = Math.sin(dLat/2)**2 + Math.cos(la1*Math.PI/180)*Math.cos(la2*Math.PI/180)*Math.sin(dLon/2)**2;
+                return 12742 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            }
+
+            function findNearestId(lon, lat) {
+                let min = Infinity, bestId = -1;
+                for (let i = 0; i < nodeCount; i++) {
+                    const d = (nodesLon[i] - lon)**2 + (nodesLat[i] - lat)**2;
+                    if (d < min) { min = d; bestId = i; }
+                }
+                return bestId;
+            }
+
             class DynamicLazyHeap {
                 constructor() { this.data = []; }
                 push(id, dist) {
